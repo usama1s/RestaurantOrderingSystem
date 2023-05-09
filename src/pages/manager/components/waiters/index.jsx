9c7aa343114cfd1bbd21e3878e64d00a458d7ffa
codethrx <tatheermehdi2000@gmail.com>
2023-05-10 01:01:31 +0500

    Form validated for add waiter part plus modal open functionality done

--- src/pages/manager/components/waiters/index.jsx
+++ src/pages/manager/components/waiters/index.jsx
@@ -1,3 +1,17 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { useCtx } from "../../../../context/Ctx";
+import { AddWaiters } from "./add-waiter";
 export function ManagersWaiterSection() {
-  return <div>Waiterx</div>;
+  const { updateModalStatus } = useCtx();
+  return (
+    <div>
+      <div className="flex items-center justify-between py-4">
+        <h1 className="text-2xl font-bold">Waiter</h1>
+        <PlusIcon
+          className="h-8 w-8 text-black cursor-pointer"
+          onClick={() => updateModalStatus(true, <AddWaiters />)}
+        />
+      </div>
+    </div>
+  );
 }
