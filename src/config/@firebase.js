360dfdc08bb224f9178d51781bfee3b4567352c6
codethrx <tatheermehdi2000@gmail.com>
2023-05-09 23:46:10 +0500

    Authentication for Admin, Manager and Waiter done with routing and route protection

--- src/config/@firebase.js
+++ src/config/@firebase.js
@@ -2,14 +2,30 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD6h0rfuUsBltgmnH4jbNs3GHmbuB8MmbY",
+//   authDomain: "foodorderingapp-b360f.firebaseapp.com",
+//   projectId: "foodorderingapp-b360f",
+//   storageBucket: "foodorderingapp-b360f.appspot.com",
+//   messagingSenderId: "662608382412",
+//   appId: "1:662608382412:web:859cd0d1618089c8d8f569"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyD6h0rfuUsBltgmnH4jbNs3GHmbuB8MmbY",
-  authDomain: "foodorderingapp-b360f.firebaseapp.com",
-  projectId: "foodorderingapp-b360f",
-  storageBucket: "foodorderingapp-b360f.appspot.com",
-  messagingSenderId: "662608382412",
-  appId: "1:662608382412:web:859cd0d1618089c8d8f569"
+  apiKey: "AIzaSyBrr-cfPJfun9szNvHp8K_6Z5VXcoUhLTM",
+
+  authDomain: "testproject-d0c87.firebaseapp.com",
+
+  projectId: "testproject-d0c87",
+
+  storageBucket: "testproject-d0c87.appspot.com",
+
+  messagingSenderId: "458782455435",
+
+  appId: "1:458782455435:web:d7afc70d97d33ce76f468e",
+
+  measurementId: "G-49QYF1B6D7",
 };
+
 // Initializing Firebase
 const app = initializeApp(firebaseConfig);
 // Initializing Authentication, Firestore and Storage
--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -18,7 +18,7 @@ import { ROLES } from "../utils/roles";
 import RolesComponent from "../components/roles";
 export function Router() {
   const { authStatus, authenticatedUser } = useCtx();
-
+  console.log(ROUTES.waiter);
   return (
     <>
       {authStatus && (
@@ -56,6 +56,16 @@ export function Router() {
             }
             path={ROUTES.login_manager}
           />
+          <Route
+            element={
+              !authenticatedUser && !authenticatedUser?.role ? (
+                <Login url={ROUTES.waiter} type={"Waiter"} />
+              ) : (
+                <Navigate to={ROUTES.waiter} />
+              )
+            }
+            path={ROUTES.login_waiter}
+          />
           <Route element={<h1>Unauthorized</h1>} path="/unauthorized" />
           <Route
             element={
@@ -74,52 +84,11 @@ export function Router() {
           <Route element={<RequireAuth roles={[ROLES.MANAGER]} />}>
             <Route element={<Manager />} path={ROUTES.manager} />
           </Route>
+          <Route element={<RequireAuth roles={[ROLES.WAITER]} />}>
+            <Route element={<Waiter />} path={ROUTES.waiter} />
+          </Route>
         </Routes>
       )}
     </>
   );
 }
-// {authStatus && (
-//   <div>
-//     <Routes>
-//       <Route
-//         path={ROUTES.default}
-//         element={<Navigate to={ROUTES.foodListings} />}
-//       />
-//       <Route path={ROUTES.foodListings} element={<FoodListings />} />
-//       <Route
-//         path={ROUTES.admin}
-//         element={
-//           loading ? (
-//             <h1>Loading..</h1>
-//           ) : (
-//             <ProtectedRoute>
-//               {user?.uid ? (
-//                 <Admin />
-//               ) : (
-//                 <Navigate to={ROUTES.login_admin} />
-//               )}
-//             </ProtectedRoute>
-//           )
-//         }
-//       />
-
-//       <Route
-//         path={ROUTES.login_admin}
-//         element={
-//           <ProtectedRoute loading={loading} error={error}>
-//             {!user?.uid ? (
-//               <LoginAdmin />
-//             ) : (
-//               <Navigate to={ROUTES.admin} replace />
-//             )}
-//           </ProtectedRoute>
-//         }
-//       />
-//       <Route
-//         path={ROUTES.all}
-//         element={<Navigate to={ROUTES.foodListings} />}
-//       />
-//     </Routes>
-//   </div>
-// )}
